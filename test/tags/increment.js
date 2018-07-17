406cb10b0df249924af5ee8dc344d3525c688413
harttle <yangjvn@126.com>
2018-07-17 20:06:20 +0800

    test cases for tags/increment, #76

--- test/tags/increment.js
+++ test/tags/increment.js
@@ -3,30 +3,23 @@ const chai = require('chai')
 const expect = chai.expect
 chai.use(require('chai-as-promised'))
 
-describe('tags/increment', function () {
+describe.only('tags/increment', function () {
   var liquid = Liquid()
 
-  it('should support increment', function () {
-    var src = '{% increment one %}{{one}}'
-    var ctx = {
-      one: 1
-    }
-    return expect(liquid.parseAndRender(src, ctx))
-      .to.eventually.equal('2')
+  it('should be post', function () {
+    var src = '{% increment v %} {% increment v %}'
+    return expect(liquid.parseAndRender(src)).to.eventually.equal('0 1')
   })
 
-  it('should increment undefined', function () {
-    var src = '{% increment empty %}{{empty}}'
-    return expect(liquid.parseAndRender(src))
-      .to.eventually.equal('1')
+  it('should reuse context variables', function () {
+    var src = '{% increment v %} {% increment v %}'
+    return expect(liquid.parseAndRender(src, {v: 10}))
+      .to.eventually.equal('10 11')
   })
 
-  it('should support increment multiple times', function () {
-    var src = '{% increment foo %}{%increment foo%}{{foo}}'
-    var ctx = {
-      foo: 1
-    }
-    return expect(liquid.parseAndRender(src, ctx))
-      .to.eventually.equal('3')
+  it('should be independent with assign', function () {
+    var src = '{% assign v=10 %}{%increment v%} {{v}}'
+    return expect(liquid.parseAndRender(src, {v: 10}))
+      .to.eventually.equal('0 10')
   })
 })
